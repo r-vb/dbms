b7c71b82ee96815091b1f81fe31b3c5b73e30b7d
Rahul V.B <r-vb@rahulvb.com>
2024-01-18 22:30:28 +0530

    Update university-smd.sql

--- university-smd.sql
+++ university-smd.sql
@@ -60,60 +60,60 @@ INSERTION VALUES:
 
 -- Insert values into Student table
 
-INSERT INTO Student VALUES('s1', 'John Doe', 'Computer Science');
-INSERT INTO Student VALUES('s2', 'Jane Smith', 'Electrical');
-INSERT INTO Student VALUES('s3', 'Bob Johnson', 'Mechanical');
-INSERT INTO Student VALUES('s4', 'Alice White', 'Civil');
-INSERT INTO Student VALUES('s5', 'Charlie Brown', 'Physics');
+INSERT INTO Student1 VALUES('s1', 'John Doe', 'Computer Science');
+INSERT INTO Student1 VALUES('s2', 'Jane Smith', 'Electrical');
+INSERT INTO Student1 VALUES('s3', 'Bob Johnson', 'Mechanical');
+INSERT INTO Student1 VALUES('s4', 'Alice White', 'Civil');
+INSERT INTO Student1 VALUES('s5', 'Charlie Brown', 'Physics');
 
 -- Insert values into Course table
 
-INSERT INTO Course VALUES ('c1', 3, 'T1', 'S1', 'Python');
-INSERT INTO Course VALUES ('c2', 4, 'T2', 'S2', 'Electrical');
-INSERT INTO Course VALUES ('c3', 3, 'T3', 'S3', 'Mechanical');
-INSERT INTO Course VALUES ('c4', 3, 'T4', 'S4', NULL);
-INSERT INTO Course VALUES ('c5', 3, 'T5', 'S5', NULL);
-INSERT INTO Course VALUES ('c6', 3, 'T6', 'S5', NULL);
-INSERT INTO Course VALUES ('c7', 3, 'T7', 'S5', 'Maths');
+INSERT INTO Course1 VALUES ('c1', 3, 'T1', 'S1', 'Python');
+INSERT INTO Course1 VALUES ('c2', 4, 'T2', 'S2', 'Electrical');
+INSERT INTO Course1 VALUES ('c3', 3, 'T3', 'S3', 'Mechanical');
+INSERT INTO Course1 VALUES ('c4', 3, 'T4', 'S4', NULL);
+INSERT INTO Course1 VALUES ('c5', 3, 'T5', 'S5', NULL);
+INSERT INTO Course1 VALUES ('c6', 3, 'T6', 'S5', NULL);
+INSERT INTO Course1 VALUES ('c7', 3, 'T7', 'S5', 'Maths');
 
 -- Insert values into Instructor table
-INSERT INTO Instructor VALUES ('i1', 'N1', 'D1', 'A1');
-INSERT INTO Instructor VALUES ('i2', 'N3', 'E1', 'B1');
-INSERT INTO Instructor VALUES ('i3', 'N4', 'M1', 'A1');
-INSERT INTO Instructor VALUES ('i4', 'N5', 'C1', 'A1');
-INSERT INTO Instructor VALUES ('i5', 'N6', 'P1', 'B1');
+INSERT INTO Instructor1 VALUES ('i1', 'N1', 'D1', 'A1');
+INSERT INTO Instructor1 VALUES ('i2', 'N3', 'E1', 'B1');
+INSERT INTO Instructor1 VALUES ('i3', 'N4', 'M1', 'A1');
+INSERT INTO Instructor1 VALUES ('i4', 'N5', 'C1', 'A1');
+INSERT INTO Instructor1 VALUES ('i5', 'N6', 'P1', 'B1');
 
 -- Insert values into Course_offerings table
 
-INSERT INTO Course_offerings VALUES ('r101', '2024', 'Spring', 'c1');
-INSERT INTO Course_offerings VALUES ('r101', '2024', 'Spring', 'c2');
-INSERT INTO Course_offerings VALUES ('r101', '2024', 'Spring', 'c4');
-INSERT INTO Course_offerings VALUES ('r201', '2024', 'Fall', 'c2');
-INSERT INTO Course_offerings VALUES ('r301', '2024', 'Spring', 'c3');
-INSERT INTO Course_offerings VALUES ('r401', '2024', 'Fall', 'c4');
-INSERT INTO Course_offerings VALUES ('r501', '2024', 'Spring', 'c5');
-INSERT INTO Course_offerings VALUES ('r501', '2024', 'Spring', 'c6');
-INSERT INTO Course_offerings VALUES ('r501', '2024', 'Fall', 'c7');
+INSERT INTO Course_offerings1 VALUES ('r101', '2024', 'Spring', 'c1');
+INSERT INTO Course_offerings1 VALUES ('r101', '2024', 'Spring', 'c2');
+INSERT INTO Course_offerings1 VALUES ('r101', '2024', 'Spring', 'c4');
+INSERT INTO Course_offerings1 VALUES ('r201', '2024', 'Fall', 'c2');
+INSERT INTO Course_offerings1 VALUES ('r301', '2024', 'Spring', 'c3');
+INSERT INTO Course_offerings1 VALUES ('r401', '2024', 'Fall', 'c4');
+INSERT INTO Course_offerings1 VALUES ('r501', '2024', 'Spring', 'c5');
+INSERT INTO Course_offerings1 VALUES ('r501', '2024', 'Spring', 'c6');
+INSERT INTO Course_offerings1 VALUES ('r501', '2024', 'Fall', 'c7');
 
 -- Insert values into teaches table
-INSERT INTO teaches VALUES ('i1', '2024', 'Spring', 'c1');
-INSERT INTO teaches VALUES ('i2', '2024', 'Fall', 'c2');
-INSERT INTO teaches VALUES ('i3', '2024', 'Spring', 'c3');
-INSERT INTO teaches VALUES ('i4', '2024', 'Fall', 'c4');
-INSERT INTO teaches VALUES ('i1', '2024', 'Spring', 'c2');
-INSERT INTO teaches VALUES ('i1', '2024', 'Spring', 'c4');
+INSERT INTO teaches1 VALUES ('i1', '2024', 'Spring', 'c1');
+INSERT INTO teaches1 VALUES ('i2', '2024', 'Fall', 'c2');
+INSERT INTO teaches1 VALUES ('i3', '2024', 'Spring', 'c3');
+INSERT INTO teaches1 VALUES ('i4', '2024', 'Fall', 'c4');
+INSERT INTO teaches1 VALUES ('i1', '2024', 'Spring', 'c2');
+INSERT INTO teaches1 VALUES ('i1', '2024', 'Spring', 'c4');
 
 -- Insert values into enrolls table
-INSERT INTO enrolls VALUES ('s1', '2024', 'Spring', 'c1', 'A');
-INSERT INTO enrolls VALUES ('s1', '2024', 'Spring', 'c3', 'A');
-INSERT INTO enrolls VALUES ('s1', '2024', 'Spring', 'c5', 'A');
-INSERT INTO enrolls VALUES ('s2', '2024', 'Fall', 'c2', 'B');
-INSERT INTO enrolls VALUES ('s3', '2024', 'Spring', 'c3', 'A');
-INSERT INTO enrolls VALUES ('s4', '2024', 'Fall', 'c4', 'B');
-INSERT INTO enrolls VALUES ('s5', '2024', 'Spring', 'c5', 'A');
-INSERT INTO enrolls VALUES ('s1', '2024', 'Spring', 'c2', 'A');
-INSERT INTO enrolls VALUES ('s1', '2024', 'Spring', 'c4', 'A');
-INSERT INTO enrolls VALUES ('s1', '2024', 'Spring', 'c6', 'A');
+INSERT INTO enrolls1 VALUES ('s1', '2024', 'Spring', 'c1', 'A');
+INSERT INTO enrolls1 VALUES ('s1', '2024', 'Spring', 'c3', 'A');
+INSERT INTO enrolls1 VALUES ('s1', '2024', 'Spring', 'c5', 'A');
+INSERT INTO enrolls1 VALUES ('s2', '2024', 'Fall', 'c2', 'B');
+INSERT INTO enrolls1 VALUES ('s3', '2024', 'Spring', 'c3', 'A');
+INSERT INTO enrolls1 VALUES ('s4', '2024', 'Fall', 'c4', 'B');
+INSERT INTO enrolls1 VALUES ('s5', '2024', 'Spring', 'c5', 'A');
+INSERT INTO enrolls1 VALUES ('s1', '2024', 'Spring', 'c2', 'A');
+INSERT INTO enrolls1 VALUES ('s1', '2024', 'Spring', 'c4', 'A');
+INSERT INTO enrolls1 VALUES ('s1', '2024', 'Spring', 'c6', 'A');
 
 queries
 
